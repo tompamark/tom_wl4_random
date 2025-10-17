fc2160421dace804008f026f0a28343aa4ac2823
Tompa Márk <tompa.mark@hallgato.sze.hu>
2025-10-17 12:28:53 +0200

    launch és package.xml szerkesztése

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -4,31 +4,15 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
+            package='battery_monitor',
+            executable='battery_simulator',
+            name='battery_simulator',
+            output='screen'
         ),
         Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
-            remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            ]
-        ),
-        # Node(
-        #     package='ros2_cpp_template',
-        #     executable='simple_sub_node',
-        #     output='screen',
-        # ),
-
-        # first comment
+            package='battery_monitor',
+            executable='battery_warning',
+            name='battery_warning',
+            output='screen'
+        )
     ])
